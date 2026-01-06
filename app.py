b1511d95bcaaba06c906ca77a37b26f112f1734f
Larry Lan <larry.lan@student.nhlstenden.com>
2026-01-06 15:18:06 +0100

    try fixing db pool

--- app.py
+++ app.py
@@ -53,7 +53,8 @@ def about():
         return jsonify(dict(zip(keys, values)))
 
 
+with app.app_context():
+    init_db()
+
 if __name__ == "__main__":
-    with app.app_context():
-        init_db()
     app.run(debug=True)
